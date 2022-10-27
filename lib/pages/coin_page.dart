3f0e442b7fd4ad6bf7e8a2529803b27eb971ffb0
jocgomez <dev.jocgomez@gmail.com>
2022-10-27 17:40:59 -0500

    Crypto assets list and starting with coin detail page

---
+++ lib/pages/coin_page.dart
@@ -0,0 +1,39 @@
+import 'package:coinbase_clone_flutter/model/coin.dart';
+import 'package:flutter/material.dart';
+
+class CoinPage extends StatefulWidget {
+  final Coin coin;
+  const CoinPage({required this.coin, Key? key}) : super(key: key);
+
+  @override
+  State<CoinPage> createState() => _CoinPageState();
+}
+
+class _CoinPageState extends State<CoinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          widget.coin.fullName,
+          style: const TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListView(
+            children: const [],
+          ),
+        ),
+      ),
+    );
+  }
+}
