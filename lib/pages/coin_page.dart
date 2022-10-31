36421c0090e87276eabcc09121c7dd1c08f693b1
jocgomez <dev.jocgomez@gmail.com>
2022-10-31 10:48:56 -0500

    WIP - Coin detail with interactive price chart

--- lib/pages/coin_page.dart
+++ lib/pages/coin_page.dart
@@ -1,5 +1,8 @@
 import 'package:coinbase_clone_flutter/model/coin.dart';
+import 'package:coinbase_clone_flutter/model/coin_data.dart';
+import 'package:coinbase_clone_flutter/services/coin_repository.dart';
 import 'package:coinbase_clone_flutter/util/theme.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class CoinPage extends StatefulWidget {
@@ -12,11 +15,43 @@ class CoinPage extends StatefulWidget {
 
 class _CoinPageState extends State<CoinPage> {
   late double _coinPrice;
+  late Future<List<CoinData>> _getCoinHourly;
 
   @override
   void initState() {
     super.initState();
+    _getCoinHourly = CoinRepository.getCoinHourlyData(widget.coin.ticker);
     _coinPrice = widget.coin.price;
+  }
+
+  convertToSpots(List<CoinData> coinData) {
+    final List<FlSpot> spots = [];
+    double maxPrice = 0.0;
+    double minPrice = double.maxFinite;
+    double maxTime = 0.0;
+    double minTime = double.maxFinite;
+    const normalizer = 10000;
+
+    // for loop over coin data
+    for (var i = 0; i < coinData.length; i++) {
+      final CoinData coin = coinData[i];
+      final double price = coin.high.toDouble();
+      final double time = coin.time.toDouble() / normalizer;
+      if (price > maxPrice) {
+        maxPrice = price;
+      }
+      if (price < minPrice) {
+        minPrice = price;
+      }
+      if (time > maxTime) {
+        maxTime = time;
+      }
+      if (time < minTime) {
+        minTime = time;
+      }
+      spots.add(FlSpot(time, price));
+    }
+    return [spots, maxPrice, maxTime, minPrice, minTime];
   }
 
   @override
@@ -52,6 +87,28 @@ class _CoinPageState extends State<CoinPage> {
                     fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 50),
+              FutureBuilder<List<CoinData>>(
+                future: _getCoinHourly,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.data != null) {
+                    final f = convertToSpots(snapshot.data!);
+                    return CoinHourlyChart(
+                      flspots: f[0],
+                      maxPrice: f[1],
+                      maxTime: f[2],
+                      minPrice: f[3],
+                      minTime: f[4],
+                      priceCallback: (d) {
+                        setState(() {
+                          _coinPrice = d ?? widget.coin.price;
+                        });
+                      },
+                    );
+                  }
+                  return const Text("Loading");
+                },
+              ),
               Text(
                 widget.coin.fullName,
                 style:
@@ -114,6 +171,94 @@ class _CoinPageState extends State<CoinPage> {
       style: const TextStyle(
         fontSize: 16,
         fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+}
+
+class CoinHourlyChart extends StatefulWidget {
+  final List<FlSpot> flspots;
+  final double maxPrice;
+  final double maxTime;
+  final double minPrice;
+  final double minTime;
+  final Function(double?) priceCallback;
+
+  const CoinHourlyChart(
+      {required this.flspots,
+      required this.maxPrice,
+      required this.maxTime,
+      required this.minPrice,
+      required this.minTime,
+      required this.priceCallback,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _CoinHourlyChartState createState() => _CoinHourlyChartState();
+}
+
+class _CoinHourlyChartState extends State<CoinHourlyChart> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.70,
+      child: LineChart(
+        LineChartData(
+          backgroundColor: Colors.transparent,
+          gridData: FlGridData(
+            show: false,
+            drawVerticalLine: true,
+            horizontalInterval: 1,
+            verticalInterval: 1,
+          ),
+          lineTouchData: LineTouchData(
+            enabled: true,
+            touchCallback: (c, cc) {
+              widget.priceCallback(cc?.lineBarSpots?[0].y);
+            },
+            touchTooltipData: LineTouchTooltipData(
+              tooltipBgColor: Colors.transparent,
+              tooltipPadding: const EdgeInsets.all(10),
+            ),
+          ),
+          showingTooltipIndicators: [],
+          titlesData: FlTitlesData(
+            show: true,
+            rightTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            topTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+          ),
+          borderData: FlBorderData(
+            show: true,
+            border: Border.all(color: Colors.white, width: 0),
+          ),
+          minX: widget.minTime,
+          maxX: widget.maxTime,
+          minY: widget.minPrice,
+          maxY: widget.maxPrice,
+          lineBarsData: [
+            LineChartBarData(
+              spots: widget.flspots,
+              isCurved: false,
+              color: CoinbaseTheme.color,
+              barWidth: 2,
+              isStrokeCapRound: false,
+              dotData: FlDotData(
+                show: false,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
